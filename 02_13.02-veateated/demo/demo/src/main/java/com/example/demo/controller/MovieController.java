ea602274d86df85237cc65d29f1e5a40614c6009
Tanel Metshein <tanemets@tlu.ee>
2026-02-12 21:36:47 +0200

    13_02 veateated

---
+++ 02_13.02-veateated/demo/demo/src/main/java/com/example/demo/controller/MovieController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Movie;
+import com.example.demo.repository.MovieRepository;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class MovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping("movies")
+    public List<Movie> getMovies() {
+        return movieRepository.findAll(); // leiab kõik filmid
+    }
+
+    @DeleteMapping("movies/{id}")
+    public List<Movie> deleteMovies(@PathVariable Long id) {
+        movieRepository.deleteById(id); // kustutab filmi, id järgi
+        return movieRepository.findAll();
+    }
+
+    @PostMapping("movies")
+    public List<Movie> saveMovies(@Valid @RequestBody Movie movie) {
+        movieRepository.save(movie); // salvestab
+        return movieRepository.findAll(); // uuenenud list
+    }
+}
---
+++ 02_13.02-veateated/demo/demo/src/main/java/com/example/demo/entity/Movie.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 2, max = 60, message = "Title must be 2-60 characters")
+    private String title;
+
+    @Size(min = 10, max = 300, message = "Description must be 10-300 characters")
+    private String description;
+
+    @NotBlank(message = "Director is required")
+    @Size(max = 60, message = "Director must be at most 60 characters")
+    private String director;
+
+    @NotNull(message = "Rating is required")
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 10, message = "Rating must be at most 10")
+    private Integer rating;
+}
